981f4273dd770c0ca8b2121204d46cf3dd868b40
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-06-17 08:33:27 +0300

    Delete existing directories for a clean project folder

--- linked_list/Node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    """
-    Node Class.
-    - This clas sis used to initialize a Node.
-    - A node contains a value and a pionter to next node
-    """
-    def __init(self, value):
-        self.value = value
-        self.next = None
